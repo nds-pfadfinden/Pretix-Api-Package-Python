c7f811cc431e5f8b66298d7fe14aa7c091dd4edb
Yannik-Menge <yannik.menge@gmail.com>
2026-02-17 14:08:08 +0000

    added change_answer

--- app/pretix_api/Pretix_API.py
+++ app/pretix_api/Pretix_API.py
@@ -3,10 +3,10 @@ import json
 import requests
 
 
-class Pretix_API():
-    
-    def __init__(self, organizer_url : str , api_token: str) -> None:
-        """        
+class Pretix_API:
+
+    def __init__(self, organizer_url: str, api_token: str) -> None:
+        """
         Args:
         events_url (String) -> Url for the Pretix-Organizer (default from .env file)
         api_token (String) -> API Token for the Pretix-Organizer (default from .env file)
@@ -14,57 +14,52 @@ class Pretix_API():
 
         self.s = requests.Session()
         self.config = {
-            'organizer_url': organizer_url,
-            'events_url' : f'{organizer_url}events/'
+            "organizer_url": organizer_url,
+            "events_url": f"{organizer_url}events/",
         }
         self.authHeader = {
-            "Authorization": f'Token {api_token}',
-                    "Content-Type": "application/json"
+            "Authorization": f"Token {api_token}",
+            "Content-Type": "application/json",
         }
         self.s.headers.update(self.authHeader)
-        
 
     def __del__(self) -> None:
         self.s.close()
         return
 
-
     def _check_response(self, response):
         response.raise_for_status()
         return response.json()
 
-    
     def _handle_pagination(self, url):
         data = []
         while True:
             r = self.s.get(url)
             self._check_response(r)
             data.extend(r.json()["results"])
-            
+
             if not r.json()["next"]:
                 break
             url = r.json()["next"]
-        
+
         return data
-    
+
     ### Events ###
     # GET Requests for Events
-        
+
     def get_event(self, slug):
         r = self.s.get(f'{self.config["events_url"]}{slug}/')
         return self._check_response(r)
 
-        
     def get_events(self):
         return self._handle_pagination(self.config["events_url"])
-    
-    
+
     # POST Requests for Events
-    
-    def create_event(self, file_path : str, update_dict = {}):
+
+    def create_event(self, file_path: str, update_dict={}):
         """
         method to create Events
-        
+
         can't create plugins or saleschannels
 
         Args:
@@ -74,18 +69,16 @@ class Pretix_API():
         Returns:
             response status
         """
-        
-        
+
         with open(file_path, "r") as read_file:
             data = json.load(read_file)
         data.update(update_dict)
-        
+
         r = self.s.post(self.config["events_url"], json=data)
-    
+
         return self._check_response(r)
-    
-    
-    def clone_event(self, event_slug : str, update_dict : dict):
+
+    def clone_event(self, event_slug: str, update_dict: dict):
         """
         method to Clone Events
 
@@ -98,106 +91,145 @@ class Pretix_API():
         """
 
         r = self.s.post(
-                        url=f'{self.config["events_url"]}{event_slug}/clone/',
-                        json=update_dict
-                        )
-    
+            url=f'{self.config["events_url"]}{event_slug}/clone/', json=update_dict
+        )
+
         return self._check_response(r)
-    
-    
+
     # Patch Events
-    
-    def change_event(self, event_slug : str, data : dict):
+
+    def change_event(self, event_slug: str, data: dict):
         r = self.s.patch(f'{self.config["events_url"]}{event_slug}/', json=data)
         return self._check_response(r)
-    
-    
+
     # Delete Events
 
-    def delete_event(self, event_slug : str):
+    def delete_event(self, event_slug: str):
         r = self.s.delete(f'{self.config["events_url"]}{event_slug}/')
         return self._check_response(r)
 
-    
     ### Orders ###
     # GET Requests for Orders
-        
-        
-    def get_orders(self, slug : str, content : str, filter_by_item_id = None):
-        orders = self._handle_pagination(self.config["events_url"] + slug+ "/orders?include_canceled_positions=true")
+
+    def get_orders(self, slug: str, content: str, filter_by_item_id=None):
+        orders = self._handle_pagination(
+            self.config["events_url"] + slug + "/orders?include_canceled_positions=true"
+        )
         if content == "orders":
             return orders
-        
-        
+
         positions = []
-        for  o in orders:
+        for o in orders:
             positions.extend(o["positions"])
         if filter_by_item_id:
-            positions = list(filter(lambda a: a["item"] in filter_by_item_id, positions))
+            positions = list(
+                filter(lambda a: a["item"] in filter_by_item_id, positions)
+            )
         if content == "positions":
             return positions
-        
+
         answers = []
-        for  p in positions:
+        for p in positions:
             answers.extend(p["answers"])
         if content == "answers":
             return answers
-        
+
         raise Exception("Error : Wrong Content parameter")
-        
-    
+
+    def change_answer(
+        self, event_slug: str, position_id: int, question_id: int, new_answer: str
+    ):
+
+        r = self.s.get(
+            f'{self.config["events_url"]}{event_slug}/orderpositions/{str(position_id)}/'
+        )
+
+        postions = self._check_response(r)
+
+        answers = postions.get("answers")
+
+        updated = False
+        for a in answers:
+            if int(a.get("question")) == question_id:
+                a["answer"] = new_answer
+                updated = True
+                break
+
+        if not updated:
+            answers.append({"question": question_id, "answer": new_answer})
+
+        # 3. PATCH full answers list
+        update_dict = {"answers": answers}
+        r = self.s.patch(
+            f'{self.config["events_url"]}{event_slug}/orderpositions/{str(position_id)}/',
+            json=update_dict,
+        )
+        return self._check_response(r)
+
     ### Items/Produkte ###
     # GET Items
-    
-    def get_items(self,event_slug :str):
-        items = self._handle_pagination(self.config["events_url"] + event_slug+ "/items")
+
+    def get_items(self, event_slug: str):
+        items = self._handle_pagination(
+            self.config["events_url"] + event_slug + "/items"
+        )
         return items
-    
-    
+
     # Patch Items
-    
-    def change_item(self, id : int, event_slug :str, update_dict : dict ):
-        r = self.s.patch(f'{self.config["events_url"]}{event_slug}/items/{str(id)}/',json=update_dict)
+
+    def change_item(self, id: int, event_slug: str, update_dict: dict):
+        r = self.s.patch(
+            f'{self.config["events_url"]}{event_slug}/items/{str(id)}/',
+            json=update_dict,
+        )
         return self._check_response(r)
-    
-    
+
     ### Quotas/Kontingente ###
-    
-    def get_quotas(self, event_slug : str) -> list:
-        quotas = self._handle_pagination(self.config["events_url"] + event_slug+  "/quotas/")
+
+    def get_quotas(self, event_slug: str) -> list:
+        quotas = self._handle_pagination(
+            self.config["events_url"] + event_slug + "/quotas/"
+        )
         return quotas
-    
-    def create_quota(self, event_slug: str, data : dict):
-        r = self.s.post(f'{self.config["events_url"]}{event_slug}/quotas/' ,json=data )
+
+    def create_quota(self, event_slug: str, data: dict):
+        r = self.s.post(f'{self.config["events_url"]}{event_slug}/quotas/', json=data)
         return self._check_response(r)
-    
-    def update_quota(self, event_slug : str, quota_id :int, update_dict : dict):
-        r = self.s.patch(f'{self.config["events_url"]}{event_slug}/quotas/{quota_id}/', json=update_dict)
+
+    def update_quota(self, event_slug: str, quota_id: int, update_dict: dict):
+        r = self.s.patch(
+            f'{self.config["events_url"]}{event_slug}/quotas/{quota_id}/',
+            json=update_dict,
+        )
         return self._check_response(r)
 
     ### Invoices/Rechnungen ###
-    
+
     # GET Invoices
-    
-    def get_invoices(self, event_slug : str) -> list[dict]:
-        invoices = self._handle_pagination(self.config["events_url"] + event_slug+  "/invoices/")        
+
+    def get_invoices(self, event_slug: str) -> list[dict]:
+        invoices = self._handle_pagination(
+            self.config["events_url"] + event_slug + "/invoices/"
+        )
         return invoices
-    
-    
-    def download_invoice(self, event_slug : str, invoice_number : str, path : str, invoice_filename = "") -> None:
+
+    def download_invoice(
+        self, event_slug: str, invoice_number: str, path: str, invoice_filename=""
+    ) -> None:
         if invoice_filename == "":
             invoice_filename = invoice_number
-        
-        invoice_pdf_r = self.s.get(f'{self.config["events_url"]}{event_slug}/invoices/{invoice_number}/download/' )
+
+        invoice_pdf_r = self.s.get(
+            f'{self.config["events_url"]}{event_slug}/invoices/{invoice_number}/download/'
+        )
         with open(path + f"\\{invoice_filename}.pdf", "wb") as f:
             f.write(invoice_pdf_r.content)
-            
+
         return
-            
-    
-    def download_all_invoices(self, event_slug : str, path: str) -> None:
+
+    def download_all_invoices(self, event_slug: str, path: str) -> None:
         invoices = self.get_invoices(event_slug)
         for invoice in invoices:
             self.download_invoice(event_slug, invoice["number"], path)
-            
+
         return
