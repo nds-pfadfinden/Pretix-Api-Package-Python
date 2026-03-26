bddbae5bee54ec990a9b17aeb8a02197ba8bd03f
Yannik-Menge <yannik.menge@gmail.com>
2026-03-26 13:18:07 +0000

    changed function anme

--- app/pretix_api/questions.py
+++ app/pretix_api/questions.py
@@ -23,7 +23,7 @@ class QuestionsApi(BaseAPI):
         return self._check_response(r)
 
     def get_id(self, event_slug: str, identifier: str) -> int:
-        questions = self.get_questions(event_slug=event_slug)
+        questions = self.get(event_slug=event_slug)
         identifier_norm = str(identifier).strip().lower()
         for q in questions:
             if str(q.get("identifier", "")).strip().lower() == identifier_norm:
