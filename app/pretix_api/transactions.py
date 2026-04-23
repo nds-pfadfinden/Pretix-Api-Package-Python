a8bfbb61ecc6710bd7a2a47cb0bf29c1aead27e4
Yannik-Menge <yannik.menge@gmail.com>
2026-04-23 19:42:06 +0000

    added transactions

---
+++ app/pretix_api/transactions.py
@@ -0,0 +1,19 @@
+from .base import BaseAPI
+import json
+
+
+class Transactions(BaseAPI):
+
+    def get(self, event_slug: str) -> list:
+        quotas = self._handle_pagination(
+            self.client.config["events_url"] + event_slug + "/transactions/"
+        )
+        return quotas
+
+    def get_all_for_organizer(
+        self,
+    ) -> list:
+        quotas = self._handle_pagination(
+            self.client.config["organizer_url"] + "transactions/"
+        )
+        return quotas
