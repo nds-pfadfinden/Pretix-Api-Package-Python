159af4bfdcc0d2fb71f33a69c6a963200853494b
Yannik-Menge <yannik.menge@gmail.com>
2026-04-24 14:48:00 +0000

    bugfix

--- app/pretix_api/orders.py
+++ app/pretix_api/orders.py
@@ -38,7 +38,7 @@ class OrdersApi(BaseAPI):
         new_answer: str,
     ):
 
-        question_id = self.client.questions.get(event_slug, question_identifier)
+        question_id = self.client.questions.get_by_name(event_slug, question_identifier)
 
         r = self.client.session.get(
             f'{self.client.config["events_url"]}{event_slug}/orderpositions/{str(position_id)}/'
